f23874d2efeb988932377c3f4887204935dcea15
alexander784 <alexandernyaga1999@gmail.com>
2025-04-01 17:03:03 +0300

    Soilapp model

--- soil_analysis/soil_app/models.py
+++ soil_analysis/soil_app/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class SoilAnalysis(models.Model):
+    csv_file = models.FileField(upload_to='csv_files/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Analysis_{self.id}"
